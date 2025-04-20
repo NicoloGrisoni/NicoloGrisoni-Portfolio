8ec374d92f896510c8ff27e929514eb0040f65ea
nicog <grisoninicolo@gmail.com>
2025-04-20 19:10:25 +0200

    Some upgrades have been applied to the code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,72 @@
 document.addEventListener("DOMContentLoaded", function() {
-    type()
+    //Theme
+    setPreferredThemeOfDevice()
 
+    //Change theme button
     document.getElementById('theme-toggle').addEventListener("click", function() {
         document.documentElement.classList.toggle('dark')
         updateIcon()
     })
+
+    //Menu sections responsive based on device
+    responsiveMenuSections()
+
+    //Typing on terminal
+    type()
 })
+
+function setPreferredThemeOfDevice () {
+    const systemPrefDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    if (systemPrefDark) {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+    }
+
+    updateIcon()
+}
+
+function updateIcon() {
+    const html = document.documentElement;
+    const sunIcon = document.getElementById('icon-sun');
+    const moonIcon = document.getElementById('icon-moon');
+
+    if (html.classList.contains('dark')) {
+        moonIcon.classList.add('hidden');
+        sunIcon.classList.remove('hidden');
+    } else {
+        sunIcon.classList.add('hidden');
+        moonIcon.classList.remove('hidden');
+    }
+}
+
+function responsiveMenuSections() {
+    const menuToggle  = document.getElementById("menu-toggle");
+    const mobileMenu  = document.getElementById("mobile-menu");
+
+    menuToggle.addEventListener("click", () => {
+        // fa partire l'animazione scale
+        mobileMenu.classList.toggle("scale-0");
+        mobileMenu.classList.toggle("scale-100");
+    });
+
+    // chiudi al click sul link
+    mobileMenu.querySelectorAll("a").forEach(link => {
+        link.addEventListener("click", () => {
+        mobileMenu.classList.add("scale-0");
+        mobileMenu.classList.remove("scale-100");
+        });
+    });
+
+    //
+    window.addEventListener("resize", () => {
+        if (window.innerWidth >= 768) {
+            mobileMenu.classList.add("scale-0");
+            mobileMenu.classList.remove("scale-100");
+        }
+    });
+}
 
 function type() {
     const text = "Hi! Welcome to my site";
@@ -21,20 +82,4 @@ function type() {
     }
 
     addLetter();
-}
-
-
-function updateIcon() {
-    // Theme Toggle
-    const html = document.documentElement;
-    const sunIcon = document.getElementById('icon-sun');
-    const moonIcon = document.getElementById('icon-moon');
-
-    if (html.classList.contains('dark')) {
-        moonIcon.classList.add('hidden');
-        sunIcon.classList.remove('hidden');
-    } else {
-        sunIcon.classList.add('hidden');
-        moonIcon.classList.remove('hidden');
-    }
 }
